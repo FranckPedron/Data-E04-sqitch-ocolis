1b3e3a9e93c19f7cacf77872bcc9cd0bff430f9f
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-04-01 10:46:49 +0200

    Modif des regex

--- script/deploy/3.constraints.sql
+++ script/deploy/3.constraints.sql
@@ -11,9 +11,12 @@ ALTER TABLE package
 -- je viens créer un DOMAIN qui me permet de venir customiser un type natif
 -- ici je fais en sorte que je n'ai que des entiers positifs
 CREATE DOMAIN pint AS integer CHECK (VALUE > 0);
-CREATE DOMAIN postal_code_fr AS integer CHECK (VALUE ~ '(([0-8][0-9])|(9[0-5]|97))\d{3}');
+CREATE DOMAIN postal_code_fr AS integer CHECK (VALUE ~ '(^([0-8]\d)|(9([0-5]|[7-8])))\d{3}$');
 
-CREATE DOMAIN license_plate_fr AS integer CHECK (VALUE ~ '[a-zA-Z]{2}(\d{3}|[\-|\s]\d{3})(([a-zA-Z]{2})|[\-|\s][a-zA-Z]{2})');
+CREATE DOMAIN license_plate_fr AS text CHECK (
+    VALUE ~ '^[a-zA-Z]{2}(\d{3}|[\-|\s]\d{3})(([a-zA-Z]{2})|[\-|\s][a-zA-Z]{2})')
+        AND 
+    VALUE ~ '![IOU]';
 
 ALTER TABLE package
     ALTER COLUMN width TYPE pint,
